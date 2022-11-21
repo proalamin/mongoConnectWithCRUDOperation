623cf69820461a31654a91be52a22f4bc109e32c
proalamin-247 <pro.alamin247@gmail.com>
2022-11-21 09:33:50 +0600

    setup and mongodb cloud database connected successfull

---
+++ dbConfig.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
+
+
+// Create a new MongoClient
+const client = new MongoClient(process.env.DATABASE_CLOUD);
+
+console.log('Database connected');
+
